7e284d2d45123124a38fd0a731ff2a8081571190
Sean_or_Tony <anthonyasamoah48@gmail.com>
2022-02-17 23:45:05 +0000

    Template and views now render data from db

--- Blogg/views.py
+++ Blogg/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
+from django.utils import timezone
 
 
 # Create your views here.
 
 def index(request):
-	return render(request, 'Blogg/index.html')
+	post = Post.objects.filter(date_published__lte=timezone.now()).order_by('date_published')
+	return render(request, 'Blogg/index.html', {
+		'post': post
+	})
