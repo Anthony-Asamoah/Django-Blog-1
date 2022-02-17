763bcada239e51ef04402cfc8603da2efb97f8b7
Sean_or_Tony <anthonyasamoah48@gmail.com>
2022-02-17 15:04:03 +0000

    included localhost in allowed hosts/settings

--- Blog/settings.py
+++ Blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*5q@w@3$v@j7oi_wrnv0$e!9u628fw50m-74_dh*d(c-7_miuo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'tony48.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'tony48.pythonanywhere.com']
 
 
 # Application definition
