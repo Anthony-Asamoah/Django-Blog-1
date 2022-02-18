6325a652c08fe53249141c10453c79cae3f7d243
Sean_or_Tony <anthonyasamoah48@gmail.com>
2022-02-18 00:52:13 +0000

    added styling to template and included vars from db

--- Blogg/views.py
+++ Blogg/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
 from django.utils import timezone
+from datetime import datetime
 
 
 # Create your views here.
 
 def index(request):
+	date = datetime.now()
+	day = date.strftime('%A')
+	day_num = date.strftime('%d')
 	post = Post.objects.filter(date_published__lte=timezone.now()).order_by('date_published')
 	return render(request, 'Blogg/index.html', {
-		'post': post
+		'post': post,
+		'day': day,
+		'day_num': day_num
 	})
