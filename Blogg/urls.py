ec530bfd625c1a32755ce52d049f440445485ad3
Sean_or_Tony <anthonyasamoah48@gmail.com>
2022-02-19 00:26:43 +0000

    add new post and edit post buttons inclued, with their respective templates

--- Blogg/urls.py
+++ Blogg/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('post/<int:pk>/', views.post_detail, name='post_detail'),
+	path('post/<int:pk>/edit', views.edit_post, name='edit_post'),
+	path('post/new', views.new_post, name='new_post'),
 ]
--- Blogg/views.py
+++ Blogg/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
 from django.utils import timezone
 from datetime import datetime
+from .forms import PostForm
 
 # Create your views here.
 date = datetime.now()
@@ -25,3 +26,46 @@ def post_detail(request, pk):
 		'day': day,
 		'day_num': day_num
 	})
+
+
+def new_post(request):
+	if request.method == 'POST':
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.date_published = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+		else:
+			form = PostForm
+			return render(request, 'Blogg/edit_post.html', {
+				'form': form,
+				'day': day,
+				'day_num': day_num
+			})
+
+	form = PostForm
+	return render(request, 'Blogg/edit_post.html', {
+		'form': form,
+		'day': day,
+		'day_num': day_num
+	})
+
+
+def edit_post(request, pk):
+	post = get_object_or_404(Post, pk=pk)
+	if request.method == "POST":
+		form = PostForm(request.POST, instance=post)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk)
+	else:
+		form = PostForm(instance=post)
+	return render(request, 'Blogg/edit_post.html', {'form': form,
+        'day': day,
+        'day_num': day_num
+    })
